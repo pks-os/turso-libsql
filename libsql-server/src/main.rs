4e8f4e4749489742c108eb813a9ab25cf3cb469f
ad hoc <postma.marin@protonmail.com>
2024-09-02 13:53:32 +0200

    add admin-shell subcommand

--- libsql-server/src/main.rs
+++ libsql-server/src/main.rs
@@ -280,16 +280,18 @@ struct Cli {
     /// Auth key for the admin API
     #[clap(long, env = "LIBSQL_ADMIN_AUTH_KEY", requires = "admin_listen_addr")]
     admin_auth_key: Option<String>,
+
+    #[clap(subcommand)]
+    subcommand: Option<UtilsSubcommands>,
 }
 
 #[derive(clap::Subcommand, Debug)]
 enum UtilsSubcommands {
-    Dump {
+    AdminShell {
+        #[clap(long, default_value = "http://127.0.0.1:9090")]
+        admin_api_url: String,
         #[clap(long)]
-        /// Path at which to write the dump
-        path: Option<PathBuf>,
-        #[clap(long)]
-        namespace: String,
+        namespace: Option<String>,
     },
 }
 
@@ -686,6 +688,25 @@ async fn build_server(config: &Cli) -> anyhow::Result<Server> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    if let Some(ref subcommand) = args.subcommand {
+        match subcommand {
+            UtilsSubcommands::AdminShell {
+                admin_api_url,
+                namespace,
+            } => {
+                let client =
+                    libsql_server::admin_shell::AdminShellClient::new(admin_api_url.clone());
+                if let Some(ns) = namespace {
+                    client.run_namespace(ns).await?;
+                }
+            }
+        }
+
+        return Ok(());
+    }
+
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info");
     }
@@ -705,8 +726,6 @@ async fn main() -> Result<()> {
                 .with_filter(tracing_subscriber::EnvFilter::from_default_env()),
         )
         .init();
-
-    let args = Cli::parse();
 
     args.print_welcome_message();
     let server = build_server(&args).await?;
